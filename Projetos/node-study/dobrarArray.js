7c4b27763fcd1500aef6b93d40d0d338c1b05a19
ericrc77 <ericzezim@gmail.com>
2025-09-17 15:20:30 -0300

    ex of arrays

---
+++ Projetos/node-study/dobrarArray.js
@@ -0,0 +1,19 @@
+let array = [];
+function adicionar(numero) {
+    array.push(numero);
+}
+
+adicionar(1);
+adicionar(2);
+adicionar(3);
+
+function dobrado() {
+    return array.map(x => x * 2);
+}
+
+console.log(dobrado());
+
+// versao simples
+let arr = [1, 2, 3];
+let dobrador = arr.map(x => x * 2);
+console.log(dobrador);
